c757eaae41c006702c2192cf321dbd59124a9492
will84530 <will84530@yahoo.com.tw>
2021-04-10 11:52:43 +0800

    add particle.

--- main.lua
+++ main.lua
@@ -3,6 +3,35 @@ local RightSheet = display.newGroup()
 local MiddleSheet = display.newGroup()
 local xCenter, yCenter = display.contentCenterX, display.contentCenterY
 local wScreen, hScreen = display.actualContentWidth, display.actualContentHeight
+local emitterParams = {
+	startColorAlpha = 1,
+    startParticleSizeVariance = 53.47,
+    startColorGreen = 0.3031555,
+    yCoordFlipped = -1,
+    blendFuncSource = 770,
+    rotatePerSecondVariance = 153.95,
+    particleLifespan = 0.7237,
+    tangentialAcceleration = -144.74,
+    finishColorBlue = 0.3699196,
+    finishColorGreen = 0.5443883,
+    blendFuncDestination = 1,
+    startParticleSize = 50.95,
+    startColorRed = 0.8373094,
+    textureFileName = "Texture/sample.png",
+    startColorVarianceAlpha = 1,
+    maxParticles = 256,
+    finishParticleSize = 64,
+    duration = -1,
+    finishColorRed = 1,
+    maxRadiusVariance = 72.63,
+    finishParticleSizeVariance = 64,
+    gravityy = -671.05,
+    speedVariance = 90.79,
+    tangentialAccelVariance = -92.11,
+    angleVariance = -142.62,
+    angle = -244.11
+}
+local emitter = display.newEmitter( emitterParams )
 
 function makeSheet(params)
 	local sheet = display.newGroup()
@@ -35,5 +64,7 @@ local middleSheet = makeSheet{
 	y = yCenter,
 	color = {0.2, 0.2, 0.2}
 }
+
+middleSheet:insert(emitter)
 
 
